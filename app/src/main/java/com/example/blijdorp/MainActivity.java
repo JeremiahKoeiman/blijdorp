ac6868933af0ae0704bc1224191035abe55fa262
Jeremiah Koeiman <9001603@student.zadkine.nl>
2021-01-13 15:35:40 +0100

    trying

--- app/src/main/java/com/example/blijdorp/MainActivity.java
+++ app/src/main/java/com/example/blijdorp/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.blijdorp;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.InflateException;
+import android.view.LayoutInflater;
+import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +19,8 @@ import com.google.firebase.firestore.QuerySnapshot;
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "DocSnippets";
+    Context context = this;
+    View row_view;
 
     FirebaseFirestore db = FirebaseFirestore.getInstance();
 
@@ -31,6 +37,17 @@ public class MainActivity extends AppCompatActivity {
                     if (task.isSuccessful()) {
                         for (QueryDocumentSnapshot document : task.getResult()) {
                             Log.d(TAG, document.getId() + " => " + document.getData());
+
+                            //LayoutInflater inflater = getLayoutInflater();
+                            //View viewAnimalItem = inflater.inflate(R.layout.animal_item, null);
+
+
+                            try {
+                                LayoutInflater inflater = getLayoutInflater();
+                                row_view = inflater.createView("R.layout.animal_item", null, null);
+                            } catch (ClassNotFoundException | InflateException e) {
+                                System.out.println(e.getMessage());
+                            }
                         }
                     } else {
                         Log.w(TAG, "Error getting documents.", task.getException());
