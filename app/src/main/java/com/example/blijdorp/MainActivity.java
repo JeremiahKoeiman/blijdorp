eb3ad9747cd4e335de57c296b1e4a7003f1c9a0f
Jeremiah Koeiman <9001603@student.zadkine.nl>
2021-01-15 10:43:31 +0100

    Layout finally working. Kinda...

--- app/src/main/java/com/example/blijdorp/MainActivity.java
+++ app/src/main/java/com/example/blijdorp/MainActivity.java
@@ -1,23 +1,37 @@
 package com.example.blijdorp;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.Timestamp;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "DocSnippets";
 
-
+    ListView listView;
+    String[] mId = new String[3];
+    String[] mName = new String[3];
+    String[] mVideoUrl = new String[3];
+    String[] mFeedingTime = new String[3];
 
     FirebaseFirestore db = FirebaseFirestore.getInstance();
 
@@ -28,23 +42,75 @@ public class MainActivity extends AppCompatActivity {
 
         db.collection("dieren")
             .get()
-            .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                @Override
-                public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                    if (task.isSuccessful()) {
-                        for (QueryDocumentSnapshot document : task.getResult()) {
-                            Log.d(TAG, document.getId() + " => " + "naam: " + document.get("naam") + ", video_url: " + document.get("video_url") + ", voedertijd: " + document.get("voedertijd"));
+            .addOnCompleteListener(task -> {
+                if (task.isSuccessful()) {
 
-                            Timestamp timestamp = (Timestamp) document.get("voedertijd");
-                            String yeah = timestamp.toDate().toString();
+                    for (QueryDocumentSnapshot document : task.getResult()) {
 
-                            Log.d(TAG, yeah);
+                        int docId = Integer.parseInt(document.getId());
 
-                        }
-                    } else {
-                        Log.w(TAG, "Error getting documents.", task.getException());
+                        Timestamp timestamp = (Timestamp) document.get("voedertijd");
+                        String formattedTimestamp = timestamp.toDate().toString();
+
+                        mId[docId] = Integer.toString(docId);
+                        mName[docId] = (String) document.get("naam");
+                        mVideoUrl[docId] = (String) document.get("video_url");
+                        mFeedingTime[docId] = formattedTimestamp;
+
                     }
+                } else {
+                    Log.w(TAG, "Error getting documents.", task.getException());
                 }
             });
+
+        listView = findViewById(R.id.listView);
+        MyAdapter adapter = new MyAdapter(this, mId, mName, mFeedingTime, mVideoUrl);
+        listView.setAdapter(adapter);
+
+        Log.d(TAG, "Adapter : " + adapter);
     }
+
+    class MyAdapter extends ArrayAdapter<String> {
+
+        Context context;
+        String[] rId;
+        String[] rName;
+        String[] rVideoUrl;
+        String[] rFeedingTime;
+
+        MyAdapter (Context c, String[] id, String[] name, String[] feedingTime, String[] videoUrl) {
+            super(c, R.layout.animal_item, id);
+            this.context = c;
+            this.rId = id;
+            this.rName = name;
+            this.rVideoUrl = videoUrl;
+            this.rFeedingTime = feedingTime;
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+            //LayoutInflater inflater = (LayoutInflater) context.getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            LayoutInflater inflater = LayoutInflater.from(context);
+            View row = inflater.inflate(R.layout.animal_item, parent, false);
+
+            // TODO: the card gets the id of the document
+            CardView myCard = row.findViewById(R.id.card_view);
+
+            // TODO: The button sets the visibility of the Webview
+            Button myShowVideo = row.findViewById(R.id.showVideo);
+
+            TextView myAnimalName = row.findViewById(R.id.animalName);
+            TextView myFeedingTime = row.findViewById(R.id.feedingTime);
+
+            // TODO: show video
+            WebView myDisplayVideo = row.findViewById(R.id.displayVideo);
+
+            myAnimalName.setText(rName[position]);
+            myFeedingTime.setText(rFeedingTime[position]);
+
+            return row;
+        }
+    }
+
 }
