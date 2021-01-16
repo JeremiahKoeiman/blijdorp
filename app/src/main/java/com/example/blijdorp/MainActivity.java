94f3f22dbf62fe3391c027e93513d4a842d10dbb
Jeremiah Koeiman <9001603@student.zadkine.nl>
2021-01-16 01:16:44 +0100

    It's finally working!!!!!

--- app/src/main/java/com/example/blijdorp/MainActivity.java
+++ app/src/main/java/com/example/blijdorp/MainActivity.java
@@ -1,12 +1,15 @@
 package com.example.blijdorp;
 
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
@@ -21,17 +24,15 @@ import com.google.firebase.Timestamp;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 
-import java.util.ArrayList;
-
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "DocSnippets";
 
     ListView listView;
-    String[] mId = new String[3];
-    String[] mName = new String[3];
-    String[] mVideoUrl = new String[3];
-    String[] mFeedingTime = new String[3];
+    String[] mId = new String[4];
+    String[] mName = new String[4];
+    String[] mVideoUrl = new String[4];
+    String[] mFeedingTime = new String[4];
 
     FirebaseFirestore db = FirebaseFirestore.getInstance();
 
@@ -41,27 +42,26 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         db.collection("dieren")
-            .get()
-            .addOnCompleteListener(task -> {
-                if (task.isSuccessful()) {
+                .get()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
 
-                    for (QueryDocumentSnapshot document : task.getResult()) {
+                        for (QueryDocumentSnapshot document : task.getResult()) {
 
-                        int docId = Integer.parseInt(document.getId());
+                            int docId = Integer.parseInt(document.getId());
 
-                        Timestamp timestamp = (Timestamp) document.get("voedertijd");
-                        String formattedTimestamp = timestamp.toDate().toString();
+                            Timestamp timestamp = (Timestamp) document.get("voedertijd");
+                            String formattedTimestamp = timestamp.toDate().toString();
 
-                        mId[docId] = Integer.toString(docId);
-                        mName[docId] = (String) document.get("naam");
-                        mVideoUrl[docId] = (String) document.get("video_url");
-                        mFeedingTime[docId] = formattedTimestamp;
-
+                            mId[docId] = Integer.toString(docId);
+                            mName[docId] = (String) document.get("naam");
+                            mVideoUrl[docId] = (String) document.get("video_url");
+                            mFeedingTime[docId] = formattedTimestamp;
+                        }
+                    } else {
+                        Log.w(TAG, "Error getting documents.", task.getException());
                     }
-                } else {
-                    Log.w(TAG, "Error getting documents.", task.getException());
-                }
-            });
+                });
 
         listView = findViewById(R.id.listView);
         MyAdapter adapter = new MyAdapter(this, mId, mName, mFeedingTime, mVideoUrl);
@@ -78,7 +78,7 @@ public class MainActivity extends AppCompatActivity {
         String[] rVideoUrl;
         String[] rFeedingTime;
 
-        MyAdapter (Context c, String[] id, String[] name, String[] feedingTime, String[] videoUrl) {
+        MyAdapter(Context c, String[] id, String[] name, String[] feedingTime, String[] videoUrl) {
             super(c, R.layout.animal_item, id);
             this.context = c;
             this.rId = id;
@@ -90,7 +90,6 @@ public class MainActivity extends AppCompatActivity {
         @NonNull
         @Override
         public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-            //LayoutInflater inflater = (LayoutInflater) context.getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             LayoutInflater inflater = LayoutInflater.from(context);
             View row = inflater.inflate(R.layout.animal_item, parent, false);
 
@@ -111,6 +110,7 @@ public class MainActivity extends AppCompatActivity {
 
             return row;
         }
+
     }
 
 }
